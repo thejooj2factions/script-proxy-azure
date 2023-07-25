ca5d897d1c74ac873391bdfafa7b03cc317a5dc3
thejooj2factions <140547395+thejooj2factions@users.noreply.github.com>
2023-07-25 11:31:10 -0700

    Update automate.py

--- automate.py
+++ automate.py
@@ -1,29 +1,27 @@
-import subprocess
+import os
 
-# Define as configurações do proxy
+# Defina as configurações da proxy
 proxy_port = 3128
-allowed_networks = ["192.168.0.0/24", "10.0.0.0/8"]  # Exemplo de redes permitidas
+allowed_networks = ["192.168.0.0/24", "10.0.0.0/8"]
 
-# Define as regras de acesso
+# Configure as regras de acesso
 access_rules = [
-    "http_access allow localhost",
-    "http_access allow all"
+    "allow all",
+    "deny all"
 ]
 
-# Define o caminho do arquivo de configuração do Squid
+# Caminho para o arquivo de configuração do Squid
 squid_conf_path = "/etc/squid/squid.conf"
 
-# Adiciona as regras de acesso ao arquivo de configuração
-with open(squid_conf_path, "a") as squid_conf:
-    squid_conf.write("\n")
-    squid_conf.write("# Configuração adicionada automaticamente\n")
-    squid_conf.write(f"http_port {proxy_port}\n")
-    for rule in access_rules:
-        squid_conf.write(f"{rule}\n")
-    squid_conf.write("\n")
-    for network in allowed_networks:
-        squid_conf.write(f"acl allowed_networks src {network}\n")
-        squid_conf.write(f"http_access allow allowed_networks\n")
+# Atualize o arquivo de configuração do Squid
+with open(squid_conf_path, "w") as f:
+    f.write(f"http_port {proxy_port}\n")
+    f.write("acl allowed_networks src ")
+    f.write(" ".join(allowed_networks))
+    f.write("\n")
+    f.write("http_access ")
+    f.write("\nhttp_access ".join(access_rules))
+    f.write("\n")
 
-# Reinicia o serviço do Squid
-subprocess.run(["systemctl", "restart", "squid"])
+# Reinicie o serviço do Squid
+os.system("systemctl restart squid")
